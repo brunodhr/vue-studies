8da0e3ff0e2c75bc887fbaf5e095a23512713e22
Bruno <brunofiliperamos@hotmail.com>
2019-08-14 15:47:41 -0300

    feat: add pages structure

---
+++ my-components/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src='@/assets/logo.png' alt='logo-vue'>
+    <Hello msg='Welcome to Your Vue.js App' />
+  </div>
+</template>
+
+<script>
+import Hello from '@/components/HelloWorld.vue'
+export default {
+  name: 'Home',
+  components: {
+      Hello
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
