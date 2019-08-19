52c9b3664037a881c2d846f9dbe490f237d4e05b
Bruno <brunofiliperamos@hotmail.com>
2019-08-19 17:04:53 -0300

    refac: Button component

--- my-components/src/components/Button/index.vue
+++ my-components/src/components/Button/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="button">
-
-  </div>
+  <button class="styled">{{text}}</button>
 </template>
 
 <script>
 export default {
-  name: 'Button'
+  name: 'Button',
+  props:{
+    text: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    rounded: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed () {
+    
+  }
 }
 </script>
 
 <style scoped>
-
+.styled{
+  font-size:20px;
+  background-color:red;
+}
 </style>
