cb7055f776790b59fa20c4e3e846f1462157bae9
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-11-26 09:59:43 -0300

    feat(Index): add Layout to wrap the index page

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">index</h1>
-      <NuxtLink to="/about">Go to about</NuxtLink>
-      <div class="links">
-        <a href="about" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <layout>
+    <div slot="content">
+      <div class="container">
+        <logo />
+        <NuxtLink to="/about">Go to about</NuxtLink>
       </div>
     </div>
-  </div>
+  </layout>
 </template>
 
 <script>
 import Logo from "~/components/Logo.vue";
+import Layout from "~/components/Layout.vue";
 
 export default {
   components: {
-    Logo
+    Logo,
+    Layout
   }
 };
 </script>
@@ -30,27 +29,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
