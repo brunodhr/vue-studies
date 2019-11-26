3998f4445007743548a12fb8afd13669d736ecf3
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-11-26 09:59:25 -0300

    feat(Layout): add default comp

---
+++ nuxt/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Header />
+    <content>
+      <slot name="content"></slot>
+    </content>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+import Footer from "~/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
