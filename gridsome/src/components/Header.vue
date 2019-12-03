90f678b06d429cd14d6ef66d09592b35ff165474
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-12-03 16:32:50 -0300

    feat(Header): initial styles to comp.

--- gridsome/src/components/Header.vue
+++ gridsome/src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="header">
-    <strong>
-      <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-    </strong>
+    <g-link to="/">
+      <g-image src="~/favicon.png" width="50" />
+    </g-link>
     <nav class="nav">
-      <g-link class="nav__link" to="/">Home</g-link>
-      <g-link class="nav__link" to="/about/">About</g-link>
+      <g-link class="link" to="/">{{ $static.metadata.siteName }}</g-link>
+      <g-link class="link" to="/about/">About</g-link>
     </nav>
   </header>
 </template>
@@ -20,15 +20,19 @@
 
 <style scoped lang="scss">
 .header {
+  padding: 0 15%;
   background-color: black;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
 }
 
-.nav__link {
+.link {
+  color: white;
   margin-left: 20px;
+  transition: 0.3s;
+  &:hover {
+    color: #42b883;
+  }
 }
 </style>
