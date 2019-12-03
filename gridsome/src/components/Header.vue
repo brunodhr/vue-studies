b57e4f5bfbf1e914739164fe6b39bb1f5ba663e2
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-12-03 16:12:32 -0300

    feat(Header): initial component

---
+++ gridsome/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <strong>
+      <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+    </strong>
+    <nav class="nav">
+      <g-link class="nav__link" to="/">Home</g-link>
+      <g-link class="nav__link" to="/about/">About</g-link>
+    </nav>
+  </header>
+</template>
+
+<static-query>
+  query {
+    metadata {
+      siteName
+    }
+  }
+</static-query>
+
+<style scoped lang="scss">
+.header {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
