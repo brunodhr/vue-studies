9b812a159ae7928670748f1b600be1b0505ca231
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-12-11 17:10:21 -0300

    fix: all lint errors by the new prettier rule

--- general/.eslintrc.js
+++ general/.eslintrc.js
@@ -3,32 +3,32 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
-        "singleQuote": true,
-        "semi": false,
-        "trailingComma": "none"
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
         // prettier config in here :)
       }
     ],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
       }
     }
   ]
-};
+}
