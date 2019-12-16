d2940ec541232379da005e36d0e3da3e9d3c3de2
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-12-16 14:28:16 -0300

    fix: rm noused code

--- vuex/src/components/Counter.vue
+++ vuex/src/components/Counter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
     <h1>{{ counter }}</h1>
     <button v-on:click="addCounter">Adicionar</button>
@@ -29,21 +29,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- vuex/src/components/Counterv2.vue
+++ vuex/src/components/Counterv2.vue
@@ -14,7 +14,6 @@ export default {
       return this.$store.state.counter
     }
   },
-  mounted() {},
   methods: {
     increment() {
       this.$store.commit('increment')
@@ -25,21 +24,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
