60a451fe03adc48d315888519118c16a24778da3
Bruno Filipe <brunofiliperamos@hotmail.com>
2019-12-16 11:01:36 -0300

    feat: add func to test the another way to access store and todos config

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -3,18 +3,35 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
-    counter: 0
+    counter: 0,
+    todos: ['teste1', 'teste2']
   },
   mutations: {
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    },
     ADD_COUNTER: state => {
-      state.counter += 1
+      state.counter++
     },
     REMOVE_COUNTER: state => {
-      state.counter -= 1
+      state.counter--
+    },
+    ADD_TODO: (state, payload) => {
+      console.log('chegou aqui')
+      state.todos.push(payload)
     }
   },
-  actions: {},
+  actions: {
+    RM_TODO: (context, todo) => {
+      context.commit('REMOVE_TODO', todo)
+    }
+  },
   modules: {}
 })
+
+export default store
