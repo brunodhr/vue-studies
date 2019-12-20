3b2057e8e3088aeb05ee21a574830834790167a6
Bruno <brunofiliperamos@hotmail.com>
2019-12-20 18:53:03 -0300

    chore: config prettier and fix all lint issues

--- graphql/.eslintrc.js
+++ graphql/.eslintrc.js
@@ -3,11 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+        // prettier config in here :)
+      }
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
